0a4b5d5014c8ac81abc9d5b833d06f8400177ba9
Joyce Mauricio <joyceriveramauricio@gmail.com>
2020-06-21 15:12:46 +0800

    (Added) assets

---
+++ src/assets/cardEnums.js
@@ -0,0 +1,23 @@
+import card0 from "./_background/BTI_bg.svg";
+import card1 from "./_background/IM-sports_bg.svg";
+import card2 from "./_background/sports_bg.svg";
+import card3 from "./_background/AG_bg.svg";
+import card4 from "./_background/EB_bg.svg";
+import card5 from "./_background/IM-esports_bg.svg";
+import card6 from "./_background/poker_bg.svg";
+import card7 from "./_background/CQ_bg.svg";
+import card8 from "./_background/PT_bg.svg";
+import card9 from "./_background/MG_bg.svg";
+
+export const cards = [
+  card0,
+  card1,
+  card2,
+  card3,
+  card4,
+  card5,
+  card6,
+  card7,
+  card8,
+  card9,
+];
---
+++ src/assets/iconEnums.js
@@ -0,0 +1,27 @@
+import customerService from "./_icon/customer-service.svg";
+import customerService_on from "./_icon/customer-service_on.svg";
+import home from "./_icon/home.svg";
+import home_on from "./_icon/home_on.svg";
+import profile from "./_icon/profile.svg";
+import profile_on from "./_icon/profile_on.svg";
+import promotion from "./_icon/promotion.svg";
+import promotion_on from "./_icon/promotion_on.svg";
+import deposit from "./_icon/deposit.svg";
+import messages from "./_icon/messages.svg";
+import transfer from "./_icon/transfer.svg";
+import withdrawal from "./_icon/withdrawal.svg";
+
+export default {
+  customerService,
+  customerService_on,
+  home,
+  home_on,
+  profile,
+  profile_on,
+  promotion,
+  promotion_on,
+  deposit,
+  messages,
+  transfer,
+  withdrawal,
+};
